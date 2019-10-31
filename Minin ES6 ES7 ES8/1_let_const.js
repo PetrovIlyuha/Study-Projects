d28d583761a2d32f9e353e0bbfeac66dae3087ab
Ilya Petrov <dnamix1@gmail.com>
2019-10-31 13:34:32 +0300

    let & const in JS

--- Minin ES6 ES7 ES8/1_let_const.js
+++ Minin ES6 ES7 ES8/1_let_const.js
@@ -6,21 +6,28 @@
 
 // console.log(a);
 
-// Hoisting is not working for LET and CONST variable declaration
-function hoisted() {
-  age = 30;
-}
-let age;
-hoisted();
-console.log(age);
+// // Hoisting is not working for LET and CONST variable declaration
+// function hoisted() {
+//   age = 30;
+// }
+// let age;
+// hoisted();
+// console.log(age);
 
 // Const
-const COLOR = "#fff";
-COLOR = "#333"; // Assignment to constant variable in not allowed
+// const COLOR = "#fff";
+// COLOR = "#333"; // FIXME: Assignment to constant variable in not allowed
 
-console.log(COLOR);
+// console.log(COLOR);
 
 const array = [1, 2, 3, 4, 5];
 console.log(array);
 
 array.push(6);
+console.log(array);
+
+const obj = { name: "John" };
+
+obj.age = 23;
+
+console.log(obj);
